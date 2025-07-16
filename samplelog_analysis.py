c82e36404a50f1999ace60845ae59b2f602ae99e
jes64-ops <jessesedzro8@gmail.com>
2025-07-16 22:30:16 +0000

    final comittedd

--- samplelog_analysis.py
+++ samplelog_analysis.py
@@ -1,10 +1,14 @@
-import re
-import ipaddress
+# Need to read the file
+# extract ip addresss
+#save the output in csv file 
+import re #import reg
+import ipaddress # import id adress for validation
 from collections import Counter
-status_code_counter = Counter()
+import csv
+status_code_counter = Counter() # starts counter from log file
 
 with open('sample-log.log') as fh:
-    logfile = fh.readlines()
+    logfile = fh.readlines() # read log files 
 
 log_pattern = re.compile(
     r'(\d{1,3}(?:\.\d{1,3}){3})\s+-\s+(\w{2})\s+-\s+'
@@ -13,15 +17,15 @@ log_pattern = re.compile(
     r'(\d{3})\s+(\d+)\s+"([^"]*)"\s+"([^"]*)"\s+(\d+)'  # Status, size, referer, UA, last number
 )
 
-valid_ip = []
-possible_bot_ip = []
-post_requests =[]
+valid_ip = [] # summarises id valid
+possible_bot_ip = [] # lists id non valid
+post_requests =[]# lists posts requests
 
 
 for line in logfile:
     match = log_pattern.match(line)
-    if match:
-        ip_address = match.group(1)
+    if match: # extraction from log information 
+        ip_address = match.group(1) 
         country_code = match.group(2)
         timestamp = match.group(3)
         method = match.group(4)
@@ -42,24 +46,30 @@ for line in logfile:
             valid_ip.append(ip_address)
         except ValueError:
             possible_bot_ip.append(ip_address)
-        if method == "POST":
-            if status_code == "429":
+        if method == "POST": # if request is post
+            if status_code == "429": # status code is sorted and transfered to post list
                 post_requests.append({
                     'ip': match.group(1),
                     'timestamp': match.group(3),
                     'url': match.group(5),
                     'software': match.group(10)
-                
-
             })
-print("Valid IPs:")
+# Save bots POST requests to CSV
+with open('bot_post_requests.csv', 'w', newline='') as csvfile:
+    fieldnames = ['ip', 'timestamp', 'url', 'software']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for entry in post_requests:
+        writer.writerow(entry)
+
+print("Valid IPs:") # output valid ip address
 print(valid_ip)
 print("Possible Bot IPs :")
-print(possible_bot_ip)
+print(possible_bot_ip)# output invalid ip address suggest a bot 
 
 for entry in post_requests:
-    print(f"IP: {entry['ip']}, Time: {entry['timestamp']}, URL: {entry['url']}, SOftware Used: {entry['software']}")
+    print(f"IP: {entry['ip']}, Time: {entry['timestamp']}, URL: {entry['url']}, SOftware Used: {entry['software']}") # print information if the post requests
 
-print("Status Code Frequencies:")
+print("Status Code Frequencies:") 
 for code, count in status_code_counter.items():
-    print(f"{code}: {count} times")
+    print(f"{code}: {count} times") # displays the number of statuscodes logs
